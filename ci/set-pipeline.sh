f34ce07f3e93bdbec733983c6bff665d6601818d
David Timm <dtimm@pivotal.io>
2020-04-16 22:08:48 +0000

    WIP BOSH lite environment
    - Add terraform to provision BOSH lite
    - Switch over to using deployments-loggregator
    - Curlies to parens
    - Deleted ci environment tasks and scripts
    
    Signed-off-by: Caitlyn Yu <cyu@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -7,5 +7,6 @@ lpass ls > /dev/null # check that we're logged in
 fly -t denver set-pipeline \
     -p bosh-system-metrics \
     -c bosh-system-metrics.yml \
-    -l <(lpass show --notes "Shared-apm/concourse/bosh-system-metrics-creds.yml") \
+    -l <(lpass show --notes "Shared-Loggregator (Pivotal Only)/bosh-system-metrics-creds.yml") \
+    -v gcp_credentials_json="$(lpass show --notes "Shared-Loggregator (Pivotal Only)/GCP Service Account Key")" \
     -l scripts.yml
